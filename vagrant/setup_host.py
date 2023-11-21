f4476c8cbbb435956b89be6f705314b941dee793
Paul Vladislav <>
2023-11-21 14:06:26 +0000

    Added setup scipts, makefile and synced folder

---
+++ vagrant/setup_host.py
@@ -0,0 +1,4 @@
+import os
+
+if not os.path.isdir('./synced'):
+        os.system('mkdir synced')
---
+++ vagrant/setup_vagrant.sh
@@ -0,0 +1,5 @@
+sudo apt-get update -y
+
+sudo apt install -y python3-pip gdb gcc-multilib
+
+pip3 install capstone
