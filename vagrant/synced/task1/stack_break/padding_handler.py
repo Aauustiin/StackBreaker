a2d3f1f663cd9f96f0b0973a3307458f23cf9763
Paul Vladislav <>
2023-11-29 20:34:52 +0000

    woks on progs compiled w/o -static

--- vagrant/synced/task1/stack_break/padding_handler.py
+++ vagrant/synced/task1/stack_break/padding_handler.py
@@ -1,7 +1,9 @@
-import gdb_handler as gdb
-from gdb_handler import Scope
+import subprocess as subp
 import math
-import os
+import re
+from enum import Enum
+
+program = ''
 
 def createPadding(len, file='padding', pad=b'A'):
     padding = len * pad
@@ -9,6 +11,48 @@ def createPadding(len, file='padding', pad=b'A'):
     padf = open(file, 'wb')
     padf.write(padding)
     padf.close()
+
+class Scope(Enum):
+    LOW = 0
+    HIGH = 1
+    CLOSE = 2
+ 
+def checkPad(bPoint, padf, pad):
+
+    cmd = ['gdb', '--batch']
+    cmd.extend(['--ex', 'b *' + bPoint])
+    cmd.extend(['--ex', 'run ' + padf])
+    cmd.extend(['--ex', 'x/4wx $ebp'])
+    cmd.extend(['--ex', 'c'])
+    cmd.extend([program])
+
+    gdbProcess = subp.run(cmd, stdout=subp.PIPE, stderr=subp.PIPE)
+    gdbOut = gdbProcess.stdout.decode()
+
+    OWAddsPat = r'\t(0x(?:[0-9A-Fa-f][0-9A-Fa-f])+'
+    OWAddsPat += r'(?:' + pad + r'+))'
+    OWAddsPat = re.compile(OWAddsPat, re.MULTILINE)
+    matches = OWAddsPat.findall(gdbOut)
+    OWAdds = len(matches)
+    OWAddsStr = ' '.join(matches)
+
+    fullyOWAddsPat = r'\s?(0x(?:' + pad + r'){4})'
+    fullyOWAddsPat = re.compile(fullyOWAddsPat)
+    matches = fullyOWAddsPat.findall(OWAddsStr)
+    fullyOWAdds = len(matches)
+
+    partOWAdds = OWAdds - fullyOWAdds
+
+    if fullyOWAdds == 1 and partOWAdds == 0:
+        return True, Scope.CLOSE
+    
+    if fullyOWAdds == 0:
+        return False, Scope.LOW
+    
+    if fullyOWAdds == 4:
+        return False, Scope.HIGH
+    
+    return False, Scope.CLOSE
 
 def search(breakPoint, pad='A'):
     padBytes = pad.encode()
@@ -21,28 +65,28 @@ def search(breakPoint, pad='A'):
     while True:
         createPadding(guess, pad=padBytes)
 
-        print('    > Trying padding length {}...'.format(guess))
+        # print('    > Trying padding length {}...'.format(guess))
 
-        found, searchScope = gdb.checkPad(breakPoint, 'padding', padStr)
+        found, searchScope = checkPad(breakPoint, 'padding', padStr)
 
         if found: break
 
         if searchScope == Scope.LOW:
             lowLim = guess
-            print('    > Not enough padding')
+            # print('    > Not enough padding')
 
         if searchScope == Scope.HIGH:
             uppLim = guess
-            print('    > Too much padding')
+            # print('    > Too much padding')
 
         guess = math.floor( (lowLim + uppLim) / 2 )
 
         if searchScope == Scope.CLOSE:
-            print('    > Almost there...')
+            # print('    > Almost there...')
             while True:
                 guess -= 1
                 createPadding(guess, pad=padBytes)
-                found , _ = gdb.checkPad(breakPoint, 'padding', padStr)
+                found , _ = checkPad(breakPoint, 'padding', padStr)
                 if found: break
 
         if uppLim - guess <= 1:
@@ -56,6 +100,5 @@ def search(breakPoint, pad='A'):
         print('    > Padding length: {}'.format(guess))
 
     return guess
-
 
 
--- vagrant/synced/task1/stack_break/stack_break.py
+++ vagrant/synced/task1/stack_break/stack_break.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import gdb_handler as gdb
+import vulnerability_find as vuln
 import padding_handler as pad
 import sys
 
@@ -10,22 +10,29 @@ if not len(sys.argv) == 2:
 
 program = sys.argv[1]
 
-gdb.program = program
+vuln.program = program
+pad.program = program
 
-funcs = gdb.getFunctions(['static'])
+funcs = vuln.getFunctions()
 
 bPoints = []
 for f in funcs:
-    bps = gdb.getBPoints(f)
+    bps = vuln.getBPoints(f)
     bPoints.append((f, bps))
 
 print(bPoints)
 
+paddings = []
 for el in bPoints:
+    if len(el[1]) == 0: continue
     print('Function {}:'.format(el[0]))
     for bp in el[1]:
         print('  > Trying break point {}...'.format(bp))
         padLength = pad.search(bp)
+        if padLength > 0: 
+            paddings.append(padLength)
+
+print('Found {} possible paddings'.format(len(paddings)))
 
 
 
